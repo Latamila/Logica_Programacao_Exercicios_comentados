ac2cf34e77972ffb1f726795aba6f6eb38d75941
Camila <87709951+Latamila@users.noreply.github.com>
2024-03-02 10:23:05 -0300

    Update Versao_python.py

--- Versao_python.py
+++ Versao_python.py
@@ -8,3 +8,10 @@ print (sys.version)
  
 print("Versão detalhada:")
 print (sys.version_info)
+
+
+'''
+Importando a biblioteca sys temos acesso a uma série de funcionalidades que dizem respeito a integração do núcleo Python e sua interação com o sistema operacional, funcionalidades estas que podem ser consultadas na documentação.
+Para cumprir o enunciado da questão, inicialmente importamos sys por meio do comando import sys.
+Em seguida, apenas por convenção, vamos por meio de algumas funções print( ) exibir
+'''
