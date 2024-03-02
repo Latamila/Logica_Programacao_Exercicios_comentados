137f27fc72d10be273422dd06884d3fbc39b1696
Camila <87709951+Latamila@users.noreply.github.com>
2024-03-02 10:22:13 -0300

    Create Versao_python.py

---
+++ Versao_python.py
@@ -0,0 +1,10 @@
+'''Crie um programa que exibe a versão 
+atual do Python instalada no sistema: 
+ '''
+import sys
+ 
+print("Versão do núcleo Python")
+print (sys.version)
+ 
+print("Versão detalhada:")
+print (sys.version_info)
